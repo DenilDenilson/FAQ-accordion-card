737ec44c4b994bf8c121e168a223b9da22714ff7
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-13 19:50:41 -0500

    Mobile maquetado y maquillado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
     './src/**/*.{html, js}',
   ],
   theme: {
-    extend: {},
+    screen: {
+      'xl': '1440px',
+    },
+
+    extend: {
+      colors: {
+        //PRYMARI TEXT
+        'Very-dark-desaturated-blue': 'hsl(238, 29%, 16%)',
+        'Soft-red': 'hsl(14, 88%, 65%)',
+        // BG-GRADIENTS
+        'Soft-violet': 'hsl(273, 75%, 66%)',
+        'Soft-blue': 'hsl(240, 73%, 65%)',
+        // TEXT
+        'Very-dark-grayish-blue': 'hsl(237, 12%, 33%)',
+        'Dark-grayish-blue': 'hsl(240, 6%, 50%)',
+        // DIVIDERS
+        'Light-grayish-blue': 'hsl(240, 5%, 91%)',
+      },
+
+      fontFamily: {
+        'Kumbh': ['Kumbh Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
